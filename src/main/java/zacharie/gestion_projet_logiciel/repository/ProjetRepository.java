43ef7e47754a93ff1aa3d6ac2a84e39c15f0df35
zachzic <zachzic.zam@gmail.com>
2024-07-30 23:17:44 +0000

    Après création de ProjetRepository

---
+++ src/main/java/zacharie/gestion_projet_logiciel/repository/ProjetRepository.java
@@ -0,0 +1,7 @@
+package zacharie.gestion_projet_logiciel.repository;
+
+import zacharie.gestion_projet_logiciel.model.Projet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetRepository extends JpaRepository<Projet, Long> {
+}
---
+++ src/main/java/zacharie/gestion_projet_logiciel/repository/TacheRepository.java
@@ -0,0 +1,4 @@
+package zacharie.gestion_projet_logiciel.repository;
+
+public interface TacheRepository {
+}
